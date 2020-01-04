b36c549f8de700425f21dd5ba613dc5d61971b60
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-04 17:33:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|name, index| puts "#{index}. #{name}"
+  array.each_with_index{|name, index| puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|name| name.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  #are any of the words in an array longer than 4 characters
+  #return true if longer than 4, false if none longer than 4
+  array.include?()
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  if array.find("cheddar")
+    
+  else return nil
+  end
 end
