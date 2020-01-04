d030d909d24d5cc1a2ba4dda085baec34c844e49
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-04 18:36:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,11 +14,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  #look thru array and return first cheese, return nil if no cheese
- array.each do|item| 
- if item == ("cheddar" || "gouda" || "camembert")
-   return item
-  else return nil
-  end
-end
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
